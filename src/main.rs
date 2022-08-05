821bd0a1bc6b33cc37c24d23b2cae1fc643ec61b
IzeDev <jimmy.gustafsson@live.com>
2022-08-05 11:24:17 +0200

    Added bankaction

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ impl IoError {
 }
 
 mod domain {
+    use std::fmt::{self, write};
+
     pub struct BankAccount {
         balance: f32,
     }
@@ -51,20 +53,113 @@ mod domain {
         pub fn new(name: String, bank_account: BankAccount) -> BankCustomer {
             BankCustomer { name, bank_account }
         }
-        
     }
+
+    #[derive(Debug, Clone)]
+    pub enum BankAction {
+        Withdraw { command: String, code: String },
+        Deposit { command: String, code: String },
+        ReadCurrentBalance { command: String, code: String },
+        Exit { command: String, code: String },
+    }
+
+    impl TryFrom<&String> for BankAction {
+        type Error = String;
+
+        fn try_from(value: &String) -> Result<Self, Self::Error> {
+            match value.to_uppercase().as_str() {
+                "W" => Ok(BankAction::Withdraw {
+                    command: String::from("W"),
+                    code: String::from("Withdraw"),
+                }),
+                "D" => Ok(BankAction::Deposit {
+                    command: String::from("D"),
+                    code: String::from("Deposit"),
+                }),
+                "R" => Ok(BankAction::ReadCurrentBalance {
+                    command: String::from("R"),
+                    code: String::from("Read Current Balance"),
+                }),
+                "X" => Ok(BankAction::Exit {
+                    command: String::from("X"),
+                    code: String::from("Exit"),
+                }),
+                _ => Err(format!("Could not parse value '{}'!", value)),
+            }
+        }
+    }
+
+    impl fmt::Display for BankAction {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self {
+                BankAction::Withdraw { command, code } => write!(f, "{} - {}", code, command),
+                BankAction::Deposit { command, code } => write!(f, "{} - {}", code, command),
+                BankAction::ReadCurrentBalance { command, code } => {
+                    write!(f, "{} - {}", code, command)
+                }
+                BankAction::Exit { command, code } => write!(f, "{} - {}", code, command),
+            }
+        }
+    }
+
+    // impl PartialEq for BankAction {
+    //     fn eq(&self, other: &Self) -> bool {
+    //         self.to_string() == other.to_string()
+    //     }
+    // }
+
+    impl PartialEq<String> for BankAction {
+        fn eq(&self, other: &String) -> bool {
+            match self {
+                BankAction::Withdraw { command, code } => code == other,
+                BankAction::Deposit { command, code } => code == other,
+                BankAction::ReadCurrentBalance { command, code } => code == other,
+                BankAction::Exit { command, code } => code == other,
+            }
+        }
+    }
+
+
 }
 
 mod banking {
-    use crate::{domain::{BankCustomer, BankAccount}, IoError};
+    use crate::{
+        domain::{BankAccount, BankAction, BankCustomer},
+        IoError,
+    };
+
+    pub fn list_input_bounds(bounds: &Vec<BankAction>, separator: &str) -> String {
+        bounds
+        .iter()
+        .map(|b| b.to_string())
+        .collect::<Vec<String>>()
+        .join(separator)
+    }
 
     pub fn app<F1, F2>(deliver_output: F1, receive_input: F2)
     where
         F1: Fn(&str) -> (),
-        F2: Fn(&Vec<&str>) -> Result<String, IoError>,
+        F2: Fn(&Vec<BankAction>) -> Result<String, IoError>,
     {
-        let no_bounds: Vec<&str> = Vec::new();
-        let bounds = vec!["W", "D", "R", "X"];
+        let no_bounds: Vec<BankAction> = Vec::new();
+        let bounds = vec![
+            BankAction::Deposit {
+                command: String::from("Deposit"),
+                code: String::from("D"),
+            },
+            BankAction::Withdraw {
+                command: String::from("Withdraw"),
+                code: String::from("W"),
+            },
+            BankAction::ReadCurrentBalance {
+                command: String::from("Read Current Balance"),
+                code: String::from("R"),
+            },
+            BankAction::Exit {
+                command: String::from("Exit"),
+                code: String::from("X"),
+            }
+        ];
 
         // Create/login Customer
         let mut customer_name: Option<String> = None;
@@ -74,12 +169,12 @@ mod banking {
             match receive_input(&no_bounds) {
                 Ok(inp) => customer_name = Some(inp),
                 Err(err) => deliver_output(&err.to_string()),
-            }            
+            }
         }
 
         // let inital_balance;
         let mut initial_balance: Option<f32> = None;
-        while  initial_balance.is_none() {
+        while initial_balance.is_none() {
             deliver_output("What amount would you like to open your account with?");
 
             match receive_input(&no_bounds) {
@@ -88,7 +183,7 @@ mod banking {
                     Err(err) => deliver_output(&err.to_string()),
                 },
                 Err(err) => deliver_output(&err.to_string()),
-            }                
+            }
         }
 
         let account = BankAccount::new(initial_balance.unwrap());
@@ -97,14 +192,13 @@ mod banking {
         // Bankloop
         loop {
             let mut instructions = String::from("Please enter a valid action:\n");
-            instructions.push_str(bounds.join("\n").as_str());
+            instructions.push_str(list_input_bounds(&bounds, "\n").as_str());
             deliver_output(&instructions);
 
             match receive_input(&bounds) {
                 Ok(inp) => deliver_output(&format!("You wrote: {}", inp)),
                 Err(err) => deliver_output(&err.to_string()),
             }
-
         }
         loop {
             deliver_output("Please enter a letter!");
@@ -120,14 +214,13 @@ mod banking {
         }
     }
 }
-
-use banking::app;
+use banking::{app, list_input_bounds};
+use domain::BankAction;
 
 fn main() {
-    // Set up Console-Closures for the app
     let print_msg = |msg: &str| println!("{}", msg);
 
-    let get_input = |bounds: &Vec<&str>| -> Result<String, IoError> {
+    let get_input = |bounds: &Vec<BankAction>| -> Result<String, IoError> {
         let mut input = String::new();
         let read_input_attempt = stdin().read_line(&mut input);
 
@@ -138,11 +231,11 @@ fn main() {
                 Err(IoError::new("Empty input!"))
             } else if bounds.is_empty() {
                 Ok(input)
-            } else if bounds.contains(&(input.as_str())) {
+            } else if bounds.iter().any(|b| b == &input) {
                 Ok(input)
             } else {
-                let mut error_text = String::from("Not within bounds! They are: ");
-                error_text.push_str(bounds.join(", ").as_str());
+                let mut error_text = String::from("Not within bounds! They are:\n");
+                error_text.push_str(list_input_bounds(&bounds, "\n").as_str());
                 Err(IoError::new(&error_text))
             }
         } else {
