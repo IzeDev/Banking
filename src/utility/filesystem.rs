78e54580c051865f891f39eb3b1f95022768abe5
IzeDev <jimmy.gustafsson@live.com>
2022-08-19 19:42:41 +0200

    Fought with lifetimes

--- src/utility/filesystem.rs
+++ src/utility/filesystem.rs
@@ -1,16 +1,16 @@
 use std::fs;
 
-pub fn read_file_content(filename: &str) -> Vec<&str> {
-    if let Ok(metadata) = fs::metadata(filename) {
-        fs::read_to_string(filename).unwrap().split("\n").collect()
+pub fn read_file_content(filename: String) -> Vec<String> {
+    let filename2 = filename.as_str();
+    if let Ok(_metadata) = fs::metadata(filename2) {
+        fs::read_to_string(filename2).unwrap().split("\n").map(|e: &str| String::from(e)).collect()
     }
     else {
-        let empty: Vec<&str> = vec![];
-        empty
+        vec![]
     }
 }
 
-pub fn write(filename: &str, &'a mut data: Vec<&str>) {
+pub fn write<'a>(filename: String, data: &'a mut Vec<String>) {
     let mut data_to_write = read_file_content(filename);
 
     data_to_write.append(data);
